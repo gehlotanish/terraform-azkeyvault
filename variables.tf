3c8d1021cc0f2d17989520e3dffc1875a40dbc54
Anish Gehlot <35562972+gehlotanish@users.noreply.github.com>
2023-06-15 13:33:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,3 +39,15 @@ variable "kv_diagnostic_name" {
   description = "Key Vault log diagnostic name"
 }
 
+variable "network_acls" {
+  description = "Object with attributes: `bypass`, `default_action`, `ip_rules`, `virtual_network_subnet_ids`. Set to `null` to disable. See https://www.terraform.io/docs/providers/azurerm/r/key_vault.html#bypass for more information."
+  type = object({
+    bypass                     = optional(string, "None"),
+    default_action             = optional(string, "Deny"),
+    ip_rules                   = optional(list(string)),
+    virtual_network_subnet_ids = optional(list(string)),
+  })
+  default = {}
+}
+
+
